28822ddb00944d03fb5827a8751a21ae4239d02f
Nachobastianelli <nachobastianelli2003@gmail.com>
2024-09-26 16:06:45 -0300

    creando logica adentro de la entidad productos

--- src/Domain/Entities/Product.cs
+++ src/Domain/Entities/Product.cs
@@ -10,11 +10,32 @@ namespace Domain.Entities
 {
     public class Product
     {
+        private decimal _price;
+        private string _name;
+
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name {
+            get => _name;
+            set 
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException("Name cannot be empty.");
+                _name = value;
+            }
+        }
         public string Description { get; set; }
         public Category Category { get; set; }
-        public decimal Price { get; set; }
+
+        public decimal Price {
+            get => _price;
+            set 
+            {
+                if(value <= 0)
+                    throw new ArgumentException("Price must be greater than 0.");
+                _price = value;
+            } 
+        }
+
         public string ImagePath { get; set; }
         public bool IsAvailable { get; set; }
         public Sizes size { get; set; }
@@ -24,5 +45,33 @@ namespace Domain.Entities
         public List<Valoration> Valorations { get; set; } = new List<Valoration>();
 
         public Product() { }    
+
+        public void VerifyAvailable () 
+        {
+            IsAvailable = Quantity >= 1;
+        }
+
+        public void DecressQuantity(int quantity)
+        {
+            if (Quantity < quantity) 
+            {
+                throw new InvalidOperationException("Insufficient stock.");
+            }
+            else
+            {
+                Quantity -= quantity;
+                VerifyAvailable();
+            }
+
+        }
+
+        public void AddQuantity (int quantity)
+        {
+            if (quantity < 0) throw new ArgumentException("Quantity must be greater than 0.");
+            Quantity += quantity;
+            VerifyAvailable ();
+        }
+
+
     }
 }
